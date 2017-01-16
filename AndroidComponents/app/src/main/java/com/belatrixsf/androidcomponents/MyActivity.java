891b84e865ce2058a46312a4fcfa204ba46f2fd6
Carlos Piñan <carlos.pinan@gmail.com>
2017-01-16 10:36:17 -0500

    Final example

--- AndroidComponents/app/src/main/java/com/belatrixsf/androidcomponents/MyActivity.java
+++ AndroidComponents/app/src/main/java/com/belatrixsf/androidcomponents/MyActivity.java
@@ -11,6 +11,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.belatrixsf.androidcomponents.example.MyCustomService;
 
 /**
  * @author Carlos Piñan
@@ -23,13 +27,21 @@ public class MyActivity extends AppCompatActivity {
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-
+            Toast.makeText(MyActivity.this, intent.getStringExtra(MyCustomService.EXTRA_PARAM1), Toast.LENGTH_LONG).show();
         }
     };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_activity);
+        findViewById(R.id.clickButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyCustomService.startAction(MyActivity.this, "Bobby Wins!");
+            }
+        });
+
     }
 
     @Override
