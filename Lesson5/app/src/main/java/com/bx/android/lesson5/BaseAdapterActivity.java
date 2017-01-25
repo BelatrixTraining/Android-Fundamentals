0a486b2467807c1f5203eb554f4a030d324a2257
Pablo Johnson <pablo.88j@gmail.com>
2017-01-24 23:05:58 -0500

    main activity

---
+++ Lesson5/app/src/main/java/com/bx/android/lesson5/BaseAdapterActivity.java
@@ -0,0 +1,18 @@
+package com.bx.android.lesson5;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pjohnson on 24/01/17.
+ */
+
+public class BaseAdapterActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
